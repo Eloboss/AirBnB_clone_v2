28fc59eb7c9ca439236d61cd5b449b561c8a5710
Eloboss <elochukwunwaigbo@gmail.com>
2023-04-06 19:56:02 -0700

    ,task 5

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,7 +1,7 @@
-# Configures a web server for deployment of web_static.
+# Configures a web server for deployment of webstatic.
 
-# Nginx configuration file
-$nginx_conf = "server {
+# Nginx configuration
+# $nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By ${hostname};
